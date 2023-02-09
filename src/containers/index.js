d7a4b86cd6abe4d7faeb274ead9136b377cb9011
Thi Dang <dkthi2901@gmail.com>
2023-02-10 00:47:18 +0700

    feat: add home container

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,2 +1,3 @@
 export { default as MainContainer } from "./MainContainer"
 export { default as CreateContainer } from "./CreateContainer"
+export { default as HomeContainer } from "./HomeContainer"
