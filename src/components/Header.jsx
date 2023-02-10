a7cef3efec3ef902a8176563295f0088d49717fb
Thi Dang <dkthi2901@gmail.com>
2023-02-11 00:44:35 +0700

    fix: close profile menu on navigate

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -51,10 +51,14 @@ const Header = () => {
   }
 
   return (
-    <header className="fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16">
+    <header className="fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16 bg-primary">
       {/* desktop & tablet */}
       <div className="hidden md:flex w-full h-full items-center justify-between">
-        <Link to={"/"} className="flex items-center gap-2">
+        <Link
+          to={"/"}
+          className="flex items-center gap-2"
+          onClick={() => setIsMenu(false)}
+        >
           <img src={Logo} className="w-10 object-cover" alt="logo" />
           <p className="text-headingColor text-xl font-bold">City</p>
         </Link>
@@ -66,21 +70,36 @@ const Header = () => {
             exit={{ opacity: 0, x: 200 }}
             className="flex items-center gap-8"
           >
-            <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            <li
+              className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer"
+              onClick={() => setIsMenu(false)}
+            >
               Home
             </li>
-            <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            <li
+              className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer"
+              onClick={() => setIsMenu(false)}
+            >
               Menu
             </li>
-            <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            <li
+              className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer"
+              onClick={() => setIsMenu(false)}
+            >
               About Us
             </li>
-            <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            <li
+              className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer"
+              onClick={() => setIsMenu(false)}
+            >
               Service
             </li>
           </motion.ul>
 
-          <div className="relative flex items-center justify-center">
+          <div
+            className="relative flex items-center justify-center"
+            onClick={() => setIsMenu(false)}
+          >
             <MdShoppingBasket className="text-textColor text-2xl cursor-pointer" />
             <div className=" absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center">
               <p className="text-xs text-white font-semibold">2</p>
@@ -103,7 +122,10 @@ const Header = () => {
               >
                 {isAdmin && (
                   <Link to={"/create-item"}>
-                    <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">
+                    <p
+                      className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base"
+                      onClick={() => setIsMenu(false)}
+                    >
                       New Item <MdAdd />
                     </p>
                   </Link>
